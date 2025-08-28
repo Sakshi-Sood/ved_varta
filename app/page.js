1316c6ed37976e12896519725feb0004d88c6b7c
aditya-2k23 <holaaditya123@gmail.com>
2025-08-28 18:22:00 +0530

    Refactor HomePage: Add Expertise section

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
-import Hero from "./sections/Hero";
-import BlogPreview from "./sections/BlogPreview";
+import Hero from "../sections/Hero";
+import BlogPreview from "../sections/BlogPreview";
+import Expertise from "../components/Expertise";
 
 export default function Home() {
   return (
     <main className="bg-gradient-to-br from-yellow-200/80 via-yellow-50 to-amber-200/80 min-h-screen px-10 py-5">
       <Hero />
+      <div className="px-20">
+        <Expertise />
+      </div>
       <BlogPreview />
     </main>
   );
