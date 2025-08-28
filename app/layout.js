895f29101e847d1d675ffc5c431dff0edca979a1
aditya-2k23 <holaaditya123@gmail.com>
2025-08-28 18:21:59 +0530

    Fix Navbar import path

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Navbar from "./sections/Navbar";
+import Navbar from "../sections/Navbar";
 
 const poppins = Poppins({
   subsets: ["latin"],
