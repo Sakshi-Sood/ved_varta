cbbb24fb6c2fbd7d0af670e8507336de6abf040f
aditya-2k23 <holaaditya123@gmail.com>
2025-08-28 18:22:06 +0530

    Add expertiseAreas utility function

---
+++ utils/index.js
@@ -0,0 +1,32 @@
+export const expertiseAreas = [
+  {
+    icon: "/icons/vedic.png",
+    title: "Vedic Astrology",
+    description: "Birth chart analysis, planetary remedies, and life predictions",
+  },
+  {
+    icon: "/icons/gems.png",
+    title: "Gemstone Consultation",
+    description: "Scientific gemstone selection based on planetary positions",
+  },
+  {
+    icon: "/icons/vastu.png",
+    title: "Vastu Shastra",
+    description: "Traditional architecture and space harmony principles",
+  },
+  {
+    icon: "/icons/palmistry.png",
+    title: "Palmistry",
+    description: "Palm reading and analysis for life insights",
+  },
+  {
+    icon: "/icons/face_reading.png",
+    title: "Face Reading",
+    description: "Analysis of facial features for personality insights",
+  },
+  {
+    icon: "/icons/numerology.png",
+    title: "Numerology",
+    description: "Study of numbers and their influence on human life",
+  }
+];
