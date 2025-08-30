87abf5aec1332c4baccfda1621e27ca3a4af9ca7
aditya-2k23 <holaaditya123@gmail.com>
2025-08-30 22:30:57 +0530

    Enhance Expertise component for reusability and layout improvements

--- components/Expertise.jsx
+++ components/Expertise.jsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { expertiseAreas } from "../utils";
 
-const Expertise = () => {
+const Expertise = ({ title }) => {
   return (
-    <div className="bg-white rounded-2xl p-8 shadow-lg">
+    <div className="bg-white rounded-2xl py-10 px-16 shadow-lg max-w-6xl mx-auto">
       <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">
-        Areas of Expertise
+        {title || "Areas of Expertise"}
       </h2>
       <div className="grid md:grid-cols-3 gap-6">
         {expertiseAreas.map((area, index) => (
@@ -13,19 +13,21 @@ const Expertise = () => {
             key={index}
             className="bg-orange-50 hover:bg-orange-100/90 rounded-xl p-6 text-center border border-orange-300 hover:shadow-lg hover:border-orange-500 hover:scale-105 transition-all duration-300"
           >
-            <div className="text-4xl mb-4">
+            <div className="mb-4">
               <Image
                 src={area.icon}
                 alt={`${area.title} Icon`}
-                width={40}
-                height={40}
+                width={45}
+                height={45}
                 className="mx-auto"
               />
             </div>
-            <h3 className="text-xl font-bold text-gray-800 mb-3">
+            <h3 className="text-xl font-bold text-gray-800 mb-2">
               {area.title}
             </h3>
-            <p className="text-gray-600 text-sm">{area.description}</p>
+            <p className="text-gray-600 text-sm text-balance">
+              {area.description}
+            </p>
           </div>
         ))}
       </div>
