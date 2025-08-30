9df78e45576b48ef843187a27c480cc74d507a4d
aditya-2k23 <holaaditya123@gmail.com>
2025-08-30 22:30:56 +0530

    Add BannerCarousel to HomePage and adjust layout styles

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import Hero from "../sections/Hero";
 import BlogPreview from "../sections/BlogPreview";
 import Expertise from "../components/Expertise";
+import BannerCarousel from "@/components/BannerCarousel";
 
 export default function Home() {
   return (
-    <main className="bg-gradient-to-br from-yellow-200/80 via-yellow-50 to-amber-200/80 min-h-screen px-10 py-5">
-      <Hero />
+    <main className="bg-gradient-to-br from-yellow-200/80 via-yellow-50 to-amber-200/80 min-h-screen p-10">
+      <BannerCarousel />
       <div className="px-20">
         <Expertise />
       </div>
